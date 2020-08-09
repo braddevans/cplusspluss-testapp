daa596aa817e31b92774e0f963dac8cfc4f71daf
braddevans <braddevans123@gmail.com>
2020-08-10 00:46:28 +0100

    switch to using std namespace

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,7 @@
 #include "headers/Global_Headers.h"
 
 int main() {
-    log("Test Output Default");
     log("Test Output Yellow", terminal_color_code::yellow);
-    log("Test Output Green", terminal_color_code::green);
+    log("Test Output Red", terminal_color_code::red);
     return 0;
 }
--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -2,14 +2,16 @@
 #include <windows.h>
 #include "../headers/utils/utils.h"
 
-void log(const std::string &input, terminal_color_code color = terminal_color_code::white) {
+using namespace std;
+
+void log(const string &input, terminal_color_code color = terminal_color_code::white) {
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleTextAttribute(hConsole, color);
-    std::cout << input << std::endl;
+    cout << input << endl;
     SetConsoleTextAttribute(hConsole, terminal_color_code::white);
 }
 
 
 void log(const std::string &input) {
-    std::cout << input << std::endl;
+    cout << input << std::endl;
 }
